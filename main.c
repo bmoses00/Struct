7e58414e6b60663a6fc8e76a3d72d4fe40f15463
Brian <bmoses00@stuy.edu>
2019-10-16 17:31:55 -0400

    Finished

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 # include <time.h>
 # include <stdlib.h>
+# include <stdio.h>
 # include <string.h>
 
 struct food {
@@ -9,24 +10,39 @@ struct food {
 
 struct food example();
 void printStruct(struct food input);
-void modifyStruct(struct food input, char newName [256], int newPrice);
+void modifyStruct(struct food *input, char newName[256], int newPrice);
 
 int main() {
-  struct food foo = example();
-  printf("Example food struct: %s\n", foo);
-  // char[256] newName = "Pizza";
-  // int newCost = 25;
-  // modifyStruct()
-  // printf("Modified struct values to %s and %d\n", newName, newCost);
+  printf("\n\n");
 
+  struct food f0 = example();
+  printf("Example struct:\n");
+  printStruct(f0);
+
+  char newName[256] = "Pizza";
+  int newCost = 25;
+  modifyStruct(&f0, newName, newCost);
+  printf("\nModified struct values to %s and %d\nNew struct:\n", newName, newCost);
+  printStruct(f0);
+  printf("\n\n");
 }
 
 struct food example() {
   srand( time(NULL) );
   struct food example;
-  if (rand() % 3 == 0) strcpy(example.name, "Chicken");
-  if (rand() % 3 == 1) strcpy(example.name, "Rice");
-  if (rand() % 3 == 2) strcpy(example.name, "Ice Cream");
+  int randomInt = rand();
+  if (randomInt % 3 == 0) strcpy(example.name, "Chicken");
+  if (randomInt % 3 == 1) strcpy(example.name, "Rice");
+  if (randomInt % 3 == 2) strcpy(example.name, "Ice Cream");
   example.price = rand() % 50;
   return example;
+}
+
+void printStruct(struct food example) {
+  printf("Food name: %s\nCost: %d\n", example.name, example.price);
+}
+
+void modifyStruct(struct food *input, char newName[256], int newPrice) {
+  strcpy((*input).name, newName);
+  (*input).price = newPrice;
 }
